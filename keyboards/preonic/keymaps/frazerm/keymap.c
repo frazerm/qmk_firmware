fb9460c73c665884cfc494e0776d6ec036f0f8b1
Frazer McIntosh <me@frazerm.com>
2018-12-04 11:15:45 +0000

    More layered keys

--- keyboards/preonic/keymaps/frazerm/keymap.c
+++ keyboards/preonic/keymaps/frazerm/keymap.c
@@ -38,6 +38,7 @@ enum preonic_keycodes {
   LOWER,
   RAISE,
   GAMING,
+  STRING_EMAIL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,        KC_Q,     KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT},
   {ALL_T(KC_ESC), KC_A,     KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT },
   {KC_LSFT,       KC_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {KC_LCTL,       KC_LGUI,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL,       KC_LGUI,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Dvorak
@@ -102,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,        KC_QUOT,  KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SCLN},
   {ALL_T(KC_ESC), KC_A,     KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT },
   {KC_LSFT,       KC_NUBS,  KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH},
-  {KC_LCTL,       KC_LGUI,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL,       KC_LGUI,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Gaming
@@ -123,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,        KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_QUOT},
   {KC_ESC,        KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_ENT },
   {KC_LSFT,       KC_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {KC_LCTL,       KC_LGUI,  KC_LALT, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL,       KC_LGUI,  KC_LALT, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Windows Modifiers
@@ -144,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_LGUI, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LGUI, KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 /* Lower
@@ -157,15 +158,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |      |             |      | HOME |      |      | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
+  {LSFT(KC_BSLS), KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
   {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL },
   {_______, KC_LBRC, KC_LCBR, KC_LPRN, KC_UNDS, KC_EQL,  KC_PLUS, KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_NUHS, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, _______, _______, _______, LSFT(KC_BSLS), _______, _______, KC_NUHS, _______, _______, STRING_EMAIL, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______, _______, KC_END}
 },
 
 /* Raise
@@ -178,15 +179,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | HOME |      |      | End  |
  * `-----------------------------------------------------------------------------------'
  */
+
+// [_RAISE] = {
+//   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+//   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+//   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS},
+//   {_______, KC_F7,   KC_F8,   KC_F9,   KC_TILD, KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
+//   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+// },
 [_RAISE] = {
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {LSFT(KC_BSLS), KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
+  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL },
+  {_______, KC_LBRC, KC_LCBR, KC_LPRN, KC_UNDS, KC_EQL,  KC_PLUS, KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, _______},
+  {_______, _______, _______, _______, LSFT(KC_BSLS), _______, _______, KC_NUHS, _______, _______, STRING_EMAIL, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______, _______, KC_END}
 },
 
 /* Adjust (Lower + Raise)
@@ -207,7 +216,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, RESET,   _______, _______, _______, _______,  _______,  _______, _______, _______, _______, KC_DEL},
   {_______, _______, _______, _______, _______, _______,  _______,  QWERTY,  COLEMAK, DVORAK , GAMING,  WIN_MODS},
   {_______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, CALC,     CALC,     _______, _______, _______, _______, _______}
+  {_______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______}
 },
 
 };
@@ -264,6 +273,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
+        case STRING_EMAIL:
+          if (record->event.pressed) {
+            SEND_STRING("frazer.mcintosh\"360globalnetops.net");
+          }
+          return false;
       }
     return true;
 };
