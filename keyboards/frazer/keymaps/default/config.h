ce2a3b200569f7f3323547cd4855c7fcdf3242c8
Frazer McIntosh <me@frazerm.com>
2020-08-27 20:27:07 +0100

    keymap changes

--- keyboards/frazer/keymaps/default/config.h
+++ keyboards/frazer/keymaps/default/config.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#define TAPPING_TERM 150
+
+// be more lenient about how a hold is detected when interacting with other keys
+#define PERMISSIVE_HOLD
+
+// allow a hold to be used right after a tap
+#define TAPPING_FORCE_HOLD
+
+// register a tap, even if a button is released after the hold-period
+#define RETRO_TAPPING
+
 #define EE_HANDS
-
-#define RGB_MATRIX_KEYPRESSES
-
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_SIMPLE 
+// #define RGB_MATRIX_KEYPRESSES
+// #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_SIMPLE 
 
--- keyboards/frazer/keymaps/default/keymap.c
+++ keyboards/frazer/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include <split_util.h>
 
 // #ifdef PROTOCOL_LUFA
 //   #include "lufa.h"
@@ -16,6 +17,8 @@ extern rgblight_config_t rgblight_config;
 #endif
 
 extern uint8_t is_master;
+
+uint8_t is_left_hand;
 
 #define _QWERTY 0
 #define _LOWER 1
@@ -57,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
   HYPER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
   KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,       KC_NO,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-  KC_LCTL, KC_LGUI, KC_LALT, KC_F13,  KC_NO,   KC_SPC,  KC_NO,       KC_NO,   KC_SPC,  KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
+  KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, KC_NO,   KC_SPC,  KC_NO,       KC_NO,   KC_SPC,  KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
                                                LOWER,   KC_BSPC,     KC_BSPC, RAISE \
 ),
 
@@ -90,18 +93,66 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // void matrix_init_user() {
-//   rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_SIMPLE);
+//   is_left_hand = is_keyboard_left();
 // }
+
+// #ifdef OLED_DRIVER_ENABLE
+// void oled_task_user(void) {
+//     static const char PROGMEM qmk_logo[] = {
+//         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+//         0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+//         0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+//     };
+
+//     oled_write_P(qmk_logo, false);
+// }
+// #endif
 
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
 
-    oled_write_P(qmk_logo, false);
+    switch (get_highest_layer(layer_state)) {
+      case _QWERTY:
+        oled_write_P(PSTR("Default\n"), false);
+        break;
+      case _LOWER:
+        oled_write_P(PSTR("Lower\n"), false);
+        break;
+      case _RAISE:
+        oled_write_P(PSTR("Raise\n"), false);
+        break;
+      case _ADJUST:
+        oled_write_P(PSTR("Adjust\n"), false);
+        break;
+      default:
+        // Or use the write_ln shortcut over adding '\n' to the end of your string
+        oled_write_ln_P(PSTR("Undefined"), false);
+    }
+}
+#endif
+
+#ifdef ENCODER_ENABLE
+void encoder_update_user(uint8_t index, bool clockwise) {
+  switch (index) {
+    case 0:
+      if (clockwise) {
+        tap_code(KC_VOLU);
+      } else {
+          tap_code(KC_VOLD);
+      }
+      break;
+    case 1:
+      if (clockwise) {
+        tap_code(KC_MS_WH_DOWN);
+      } else {
+        tap_code(KC_MS_WH_UP);
+      }
+      break;
+    default:
+      break;
+  }
 }
 #endif
 
